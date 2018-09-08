ad2c84f00360b9a3bc9ba47d087af09cc31427d2
Dean Asadi <eng.de86@gmail.com>
2018-09-08 12:16:37 -0500

    another update

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,9 +8,9 @@ class Home extends Component {
       <div>
         <CenteredHeader>Hi, I'm Dean.</CenteredHeader>
         <p>
-          I am a full stack web developer, I have just graduated from a great
-          boot camp called DevMountain located in Dallas, TX. Learning
-          responsive frameworks, databases, and best code practice.
+          I am a full stack web developer, I graduated from a great boot camp
+          called DevMountain located in Dallas, TX. Learning responsive
+          frameworks, databases, and best code practice.
           <br />
           My objective is simply to be the best web developer that I can be and
           contribute to the technology industry all that I know and can do. I am
